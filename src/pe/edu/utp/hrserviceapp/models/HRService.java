753c4717628ba38ee2aae046b54423e1d7107857
MrTiwin <1411901@utp.edu.pe>
2016-06-03 22:11:00 -0500

    Initial commit (primeros pasos)

---
+++ src/pe/edu/utp/hrserviceapp/models/HRService.java
@@ -0,0 +1,59 @@
+package pe.edu.utp.hrserviceapp.models;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Created by GrupoUTP on 03/06/2016.
+ */
+//Conecta las entidades con la misma conexion
+public class HRService {
+    private RegionsEntity regionsEntity;
+    Connection connection;
+    DataSource dataSource;
+    private static String DATA_SOURCE = "jdbc/MySQLDataSource";
+
+    public HRService(InitialContext context){
+        try {
+            dataSource = (DataSource) context.lookup(DATA_SOURCE);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+    //Obtiene una nueva conexion
+    public boolean connect(){
+        if(connection == null){
+            try {
+                connection = dataSource.getConnection();
+                return(connection !=null);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public Connection getConnection(){
+        if(connection == null){
+            if (!connect()){
+                return null;
+            };
+        }
+        return connection;
+    }
+
+    public RegionsEntity getRegionsEntity() {
+        if(regionsEntity==null){
+            regionsEntity = new RegionsEntity();
+            regionsEntity.setConnection(getConnection());
+        }
+        return regionsEntity;
+    }
+
+    public void setRegionsEntity(RegionsEntity regionsEntity) {
+        this.regionsEntity = regionsEntity;
+    }
+}
